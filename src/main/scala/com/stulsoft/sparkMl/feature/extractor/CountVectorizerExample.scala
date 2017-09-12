97073303205cb2bc7da81fb353647c87ca4f88d4
ysden123 <ysden123@gmail.com>
2017-09-12 19:41:25 +0300

    CountVectorizerExample

--- src/main/scala/com/stulsoft/sparkMl/feature/extractor/CountVectorizerExample.scala
+++ src/main/scala/com/stulsoft/sparkMl/feature/extractor/CountVectorizerExample.scala
@@ -1,14 +1,18 @@
 package com.stulsoft.sparkMl.feature.extractor
+
 import com.stulsoft.sparkMl.util.TimeWatch
 import org.apache.spark.ml.feature.{CountVectorizer, CountVectorizerModel}
 import org.apache.spark.sql.SparkSession
+
 /** CountVectorizerExample
+  *
   * @see [[https://spark.apache.org/docs/latest/ml-features.html#countvectorizer CountVectorizer Example]]
   * @author Yuriy Stul.
   */
 object CountVectorizerExample extends App {
   test1()
-  def test1(): Unit ={
+
+  def test1(): Unit = {
     println("==>test1")
     val tw = TimeWatch()
     val spark: SparkSession = SparkSession.builder.
@@ -16,6 +20,33 @@ object CountVectorizerExample extends App {
       .appName("TF-IDF Feature Extractor Example")
       .getOrCreate()
 
+    val df = spark.createDataFrame(Seq(
+      (0, Array("a", "b", "c")),
+      (1, Array("a", "b", "b", "c", "a"))
+    )).toDF("id", "words")
+
+    println("Input:")
+    df.show()
+
+    // fit a CountVectorizerModel from the corpus
+    tw.start()
+    val cvModel: CountVectorizerModel = new CountVectorizer()
+      .setInputCol("words")
+      .setOutputCol("features")
+      .setVocabSize(3)
+      .setMinDF(2)
+      .fit(df)
+    println(s"Training takes ${tw.duration} ms.")
+
+    // alternatively, define CountVectorizerModel with a-priori vocabulary
+    val cvm = new CountVectorizerModel(Array("a", "b", "c"))
+      .setInputCol("words")
+      .setOutputCol("features")
+
+    println("Features:")
+    tw.start()
+    cvModel.transform(df).show(false)
+    println(s"Featuring takes ${tw.duration} ms.")
     spark.stop()
     println("<==test1")
   }
